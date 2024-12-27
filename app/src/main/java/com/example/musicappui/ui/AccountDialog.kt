e26561d272bf015cca0abb37d022f415816f9cd4
jerrycaffe <jerrycaffe@gmail.com>
2024-12-27 05:58:46 +0100

    still working on the music app

---
+++ app/src/main/java/com/example/musicappui/ui/AccountDialog.kt
@@ -0,0 +1,70 @@
+package com.example.musicappui.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.primarySurface
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
+
+@Composable
+fun AccountDialog(dialogOpen: MutableState<Boolean>){
+    if(dialogOpen.value){
+        AlertDialog(
+            modifier = Modifier.fillMaxWidth().background(MaterialTheme.colors.primarySurface).padding(8.dp),
+            shape = RoundedCornerShape(5.dp),
+            backgroundColor = Color.White,
+            properties = DialogProperties(
+                dismissOnBackPress = true,
+                dismissOnClickOutside = true
+            ),
+            onDismissRequest ={
+                dialogOpen.value = false
+            },
+          confirmButton = {
+              TextButton(onClick = { dialogOpen.value = false }) {
+                  Text("Confirm")
+              }
+          },
+            dismissButton = {
+                TextButton(onClick = { dialogOpen.value = false }) {
+                    Text("Dismiss")
+                }
+            },
+            title = {
+                Text(text = "Add Account")
+            },
+            text = {
+                Column(modifier = Modifier
+                    .wrapContentHeight()
+                    .padding(top = 16.dp),
+                    verticalArrangement = Arrangement.Center)
+                {
+                    TextField(
+                        modifier = Modifier.padding(top = 16.dp),
+                        label = { Text(text = "Email") },
+                        value = "",
+                        onValueChange = {})
+                    TextField(
+                        modifier = Modifier.padding(top = 8.dp),
+                        label = { Text(text = "Password") },
+                        value = "",
+                        onValueChange = {})
+                }
+            }
+        )
+    }
+}
--- app/src/main/java/com/example/musicappui/ui/MainView.kt
+++ app/src/main/java/com/example/musicappui/ui/MainView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -16,10 +17,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -50,48 +51,53 @@ fun MainView(modifier: Modifier) {
     val viewModel: MainViewModel = viewModel()
 
 
-
     //Allow us to find out on which view we currently are
     val controller: NavController = rememberNavController()
     val navBackStackEntry by controller.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
-val currentScreen = remember{
-    viewModel.currentScreen.value
-}
+    val currentScreen = remember {
+        viewModel.currentScreen.value
+    }
 
     val title = remember {
         //change to current screen.title
         mutableStateOf(currentScreen.title)
     }
+    var dialogOpen: MutableState<Boolean> = remember {
+        mutableStateOf(false)
+    }
     Scaffold(
-        topBar = { TopAppBar(title = { Text("Home") },
-            navigationIcon = {
-                IconButton(onClick = {
-                    scope.launch { scaffoldState.drawerState.open() }
-                }) {
-                    Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu")
-                }
-            }
-
-        ) }, scaffoldState = scaffoldState,
-        drawerContent = {
-            LazyColumn(Modifier.padding(16.dp)) {
-                items(screensInDrawer){
-                    item -> DrawerItem(selected = currentRoute == item.dRoute, item = item) {
-                    scope.launch { scaffoldState.drawerState.close() }
-                    if(item.dRoute == "add_account"){
-                        //open dialog
-                    }else{
-                        controller.navigate(item.dRoute)
-                        title.value = item.dTitle
+        topBar = {
+            TopAppBar(title = { Text(title.value) },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        scope.launch { scaffoldState.drawerState.open() }
+                    }) {
+                        Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu")
                     }
                 }
+
+            )
+        }, scaffoldState = scaffoldState,
+        drawerContent = {
+            LazyColumn(Modifier.padding(16.dp)) {
+                items(screensInDrawer) { item ->
+                    DrawerItem(selected = currentRoute == item.dRoute, item = item) {
+                        scope.launch { scaffoldState.drawerState.close() }
+                        if (item.dRoute == "add_account") {
+                            dialogOpen.value = true
+                        } else {
+                            controller.navigate(item.dRoute)
+                            title.value = item.dTitle
+                        }
+                    }
                 }
             }
         }
 
     ) {
-       Navigation(navController = controller, viewModel =viewModel , pd = it)
+        Navigation(navController = controller, viewModel = viewModel, pd = it)
+        AccountDialog(dialogOpen = dialogOpen)
     }
 }
 
@@ -99,31 +105,35 @@ val currentScreen = remember{
 fun DrawerItem(
     selected: Boolean,
     item: Screen.DrawerScreen,
-    onDrawerItemClicked: ()-> Unit
-){
-    val background = if(selected) Color.DarkGray else Color.White
-Row(
-    Modifier
-        .fillMaxWidth()
-        .padding(horizontal = 8.dp, vertical = 16.dp)
-        .background(background)
-        .clickable { onDrawerItemClicked() }) {
+    onDrawerItemClicked: () -> Unit
+) {
+    val background = if (selected) Color.DarkGray else Color.White
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .background(background)
+            .clickable { onDrawerItemClicked() }) {
 
-    Icon(
-        painter = painterResource(id = item.icon),
-        contentDescription = item.dTitle,
-        Modifier.padding(end = 8.dp, top = 4.dp)
-    )
-    Text(text = item.dTitle,
-        style = MaterialTheme.typography.bodySmall)
-}
+        Icon(
+            painter = painterResource(id = item.icon),
+            contentDescription = item.dTitle,
+            Modifier.padding(end = 8.dp, top = 4.dp)
+        )
+        Text(
+            text = item.dTitle,
+            style = MaterialTheme.typography.bodySmall
+        )
+    }
 }
 
 @Composable
-fun Navigation(navController: NavController, viewModel:MainViewModel, pd:PaddingValues){
-    NavHost(navController = navController as NavHostController,
-        startDestination = Screen.DrawerScreen.AddAccount.route, modifier = Modifier.padding(pd)){
-        composable(Screen.DrawerScreen.AddAccount.route) {  }
-        composable(Screen.DrawerScreen.Subscription.route) {  }
+fun Navigation(navController: NavController, viewModel: MainViewModel, pd: PaddingValues) {
+    NavHost(
+        navController = navController as NavHostController,
+        startDestination = Screen.DrawerScreen.Account.route, modifier = Modifier.padding(pd)
+    ) {
+        composable(Screen.DrawerScreen.Account.route) { AccountView() }
+        composable(Screen.DrawerScreen.Subscription.route) { Subscription()}
     }
 }
